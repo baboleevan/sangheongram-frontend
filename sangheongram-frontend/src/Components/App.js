3dde143e4f8638b80555bc2f1b41653087389f5d
rlarnfktkd1 <ksj8367@gmail.,com>
2019-05-20 20:18:10 +0900

    typescript Change

--- sangheongram-frontend/src/Components/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { ThemeProvider } from "styled-components";
-import GlobalStyles from "../Styles/GlobalStyles";
-
-export default () => (
-  <ThemeProvider theme={Theme}>
-    <GlobalStyles />
-  </ThemeProvider>
-);
